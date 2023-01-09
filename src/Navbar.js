cf819cc20c096e40c38ef74226d37016dd993464
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-01-08 17:31:19 -0800

    added avatar icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,42 +1,59 @@
 import { Link, useMatch, useResolvedPath, useLocation, NavLink } from "react-router-dom"
-import { Menu, Layout } from 'antd';
+import { Menu, Layout, Dropdown, Avatar, Badge} from 'antd';
 import {
   HomeFilled,
   FileFilled,
   FundFilled,
-  EditFilled
+  EditFilled,
+  UserOutlined
 } from '@ant-design/icons';
 import './navbar.css';
 import React, { useState } from 'react'
 
 export default function Navbar() {
     const location = useLocation();
+    const { Header, Content, Footer } = Layout;
+
     return (
       <Layout>
-        <Layout.Header className = "navbar" style={{ backgroundColor: '#ffffff' }}>
-          <Menu className = "navbar"  style={{ display: 'flex', justifyContent: 'center' }} mode="horizontal" selectedKeys={[location.pathname]}>
-            <Menu.Item key="/" className="nav-item" >
+        <Header style={{ backgroundColor: '#ffffff' }}>
+          <Menu className = "navbar"  
+            mode="horizontal" 
+            selectedKeys={[location.pathname]}>
+              
+          <Menu.Item key="/" className="nav-item" >
               <Link  to="/"> 
                 <HomeFilled style={{ fontSize: '30px' }} />
               </Link>
-            </Menu.Item>
-            <Menu.Item key="/pages/FileManager" className="nav-item" >
+          </Menu.Item>
+
+          <Menu.Item key="/pages/FileManager" className="nav-item" >
               <Link to="/pages/FileManager">
                 <FileFilled className="icon" style={{ fontSize: '30px' }} />
               </Link>
-            </Menu.Item>
-            <Menu.Item key="/pages/practice" className="nav-item" >
+          </Menu.Item>
+
+          <Menu.Item key="/pages/practice" className="nav-item" >
               <Link to="/pages/practice">
                 <EditFilled style={{ fontSize: '30px' }} /> 
               </Link>
-            </Menu.Item>
-            <Menu.Item key="/pages/dashboard" className="nav-item" >
+          </Menu.Item>
+
+          <Menu.Item key="/pages/dashboard" className="nav-item" >
               <Link to="/pages/dashboard">
                 <FundFilled style={{ fontSize: '30px' }} />
               </Link>
-            </Menu.Item>
+          </Menu.Item>
+
+          <Menu.Item key="/pages/account">
+          <Link to="/pages/account">
+            <span className="avatar-item">
+              Walter White <Avatar shape = "circle" icon = {<UserOutlined/>} size = 'large' style = {{backgroundColor: '#412973'}}/>
+            </span>
+            </Link>
+          </Menu.Item>
           </Menu>
-        </Layout.Header>
+        </Header>
       </Layout>
     );
   }
