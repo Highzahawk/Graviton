4b26a23bc1cdf08eb9c496b7901b3f6716125bd9
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-01-09 16:03:48 -0800

    dashboard screen completed

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,48 +1,131 @@
-import react from 'react';
+
+import react, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Col, Row, Statistic, Button, Card} from 'antd';
+import { Col, Row, Statistic, Button, Card, Layout, Select} from 'antd';
 import styles from "./dashboardStyles.css";
-import { Column } from '@ant-design/plots'; 
+import { Column, Line } from '@ant-design/plots'; 
+import { Content } from 'antd/es/layout/layout';
 
 
 
-    const data = [
-      { year: '1991', value: 3 },
-      { year: '1992', value: 4 },
-      { year: '1993', value: 3.5 },
-      { year: '1994', value: 5 },
-      { year: '1995', value: 4.9 },
-      { year: '1996', value: 6 },
-      { year: '1997', value: 7 },
-      { year: '1998', value: 9 },
-      { year: '1999', value: 13 },
-    ];
-    const config = {
-      data,
-      height: 400,
-      xField: 'year',
-      yField: 'value',
-      point: {
-        size: 5,
-        shape: 'diamond | circule', 
-      },
-      tooltip: {
-        formatter: (data) => {
-          return {
-            name: ''
-          };
-        },
-        customContent: (name, data) =>
-          `<div>${data?.map((item) => {
-            return `<div class="tooltip-chart" >
-                <span class="tooltip-item-name">${item?.name}</span>
-                <span class="tooltip-item-value">${item?.value}</span>
-              </div>`;
-          })}</div>`,
-      },
-    };
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
 
-    function Dashboard(){
-    return <Column {...config} />;
-    }
+const data = [
+  {
+    class: 'AP Language',
+    Grade: 85,
+  },
+  {
+    class: 'AP Psychology',
+    Grade: 90,
+  },
+  {
+    class: 'AP Calculus AB',
+    Grade: 78,
+  },
+  {
+    class: 'Engineering 1',
+    Grade: 83,
+  },
+  {
+    class: 'Graphic Production',
+    Grade: 100,
+  },
+  {
+    class: 'AP Computer Science',
+    Grade: 92,
+  },
+];
+
+const config = {
+  columnStyle: {
+    fill:'#1A6680',
+    stroke: 'black',
+    lineWidth:2,
+    cursor: 'pointer'
+  },
+  data,
+  yField: 'Grade',
+  xField: 'class',
+  tooltip: {
+    customContent: (title, items) => {
+      return (
+        <>
+          <h5 style={{ marginTop: 16 }}>{title}</h5>
+          <ul style={{ paddingLeft: 0 }}>
+            {items?.map((item, index) => {
+              const { name, value, color } = item;
+              return (
+                <li
+                  key={item.class}
+                  className="g2-tooltip-list-item"
+                  data-index={index}
+                  style={{ marginBottom: 4, display: 'flex', alignItems: 'center' }}
+                >
+                  <span className="g2-tooltip-marker" style={{ backgroundColor: color }}></span>
+                  <span
+                    style={{ display: 'inline-flex', flex: 1, justifyContent: 'space-between' }}
+                  >
+                    <span style={{ marginRight: 16 }}>{name}:</span>
+                    <span className="g2-tooltip-list-item-value">{value}</span>
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      );
+    },
+  },
+  point: {
+    size: 5,
+    shape: 'circle',
+    style: {
+      fill: 'whites',
+      stroke: '#ffffff',
+      lineWidth: 2,
+    },
+  },
+};
+
+
+
+function Dashboard() {
+  const [selectedData, setSelectedData] = useState(data);
+  const options = data.map(d => ({ value: d.class, label: d.class }));
+  const onChange = (value) => {
+    setSelectedData(data.filter(d => d.class === value));
+  }
+  return (
+    <Row gutter={[32, 20]} justify = 'center'>
+      <Col>
+      <Select className='classDropdown'
+      bordered = {false}
+      placeholder = "Select Class"
+      placement='topLeft'
+      size='large'
+        onChange={onChange}
+        options={options}
+      />
+      </Col>
+      <Col>
+      <Select className='unitDropdown'
+      bordered = {false}
+      placeholder = "Select Unit"
+      size='large'
+        onChange={onChange}
+        options={options}
+      />
+      </Col>
+      <div style={{ width: '100%', height: '80vh' }}>
+        <Column {...config} />
+        </div>
+
+      </Row>
+  );
+}
+
 export default Dashboard;
+
