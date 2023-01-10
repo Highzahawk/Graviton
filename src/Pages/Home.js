54aa52d97d2df1a5f0fdaf051e3f50c72985ff7d
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-01-09 19:54:55 -0800

    navbar changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,40 +1,25 @@
 import react from 'react';
 import { useNavigate } from 'react-router-dom';
-import {Button, Card, Space, Col, Row} from 'antd';
-import {SearchOutlined, VerticalAlignBottomOutlined} from '@ant-design/icons';
-import styles from "./buttonStyles.css";
-
 
 function Home(){
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
+
     return(
-        <Row gutter={[16, 40]}>
-            <Col span = {8}>
-                <Button type = "primary" className = "NewClass" shape = "round" size = "large">
-                    Add New Class
-                </Button>
-            </Col>
+        <div>
+            <h>
+                Home
+            </h>
 
-            <Col span = {8} offset = {8}>
-                <Button type="primary" className = "Search" shape = "round" size = "large" >
-                    <SearchOutlined />
-                    Search
-                </Button>
-            </Col>
+            <button>
+                Add new class
+            </button>
 
-            <Col span = {8}> 
-                <Button type = "default" className = "Classes" size = "large">
-                Calculus AB
-            </Button>
-            </Col>                 
-        </Row>
+            <button>
+                Search
+            </button>
 
-
-        
-     
+        </div>
     );
 }
 
-
 export default Home;
-
