80a6aa16fdb135777802092043b77fbc52853893
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-01-09 18:59:39 -0800

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,40 @@
 import react from 'react';
 import { useNavigate } from 'react-router-dom';
+import {Button, Card, Space, Col, Row} from 'antd';
+import {SearchOutlined, VerticalAlignBottomOutlined} from '@ant-design/icons';
+import styles from "./buttonStyles.css";
+
 
 function Home(){
-    const navigate = useNavigate();
-
+    const navigate = useNavigate(); 
     return(
-        <div>
-            <h>
-                Home
-            </h>
+        <Row gutter={[16, 40]}>
+            <Col span = {8}>
+                <Button type = "primary" className = "NewClass" shape = "round" size = "large">
+                    Add New Class
+                </Button>
+            </Col>
 
-            <button>
-                Add new class
-            </button>
+            <Col span = {8} offset = {8}>
+                <Button type="primary" className = "Search" shape = "round" size = "large" >
+                    <SearchOutlined />
+                    Search
+                </Button>
+            </Col>
 
-            <button>
-                Search
-            </button>
+            <Col span = {8}> 
+                <Button type = "default" className = "Classes" size = "large">
+                Calculus AB
+            </Button>
+            </Col>                 
+        </Row>
 
-        </div>
+
+        
+     
     );
 }
 
+
 export default Home;
+
