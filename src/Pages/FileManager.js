437bd111d0abbc89cbdcfdf8b18456f96f06e400
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-01-19 12:39:39 -0600

    filter button created

--- src/Pages/FileManager.js
+++ src/Pages/FileManager.js
@@ -1,22 +1,47 @@
 import react from 'react';
 import { useNavigate } from 'react-router-dom';
+import {DownloadOutlined, FilterFilled} from '@ant-design/icons'
+import {Button, Layout, Space, Grid, Row, Col, Dropdown, Typography} from 'antd'
 
 function FileManager(){
     const navigate = useNavigate();
+
+    const items = [
+    {
+        key: '1',
+        label: 'Item 1',
+    },
+    {
+        key: '2',
+        label: 'Item 2',
+    },
+    {
+        key: '3',
+        label: 'Item 3',
+    },
+  ];
+
     return(
-        <div>
-            <h>
-                File Manager Page
-                <button>download content</button>
-            </h>
-
-            <button>Filter</button>
-            <button>Unit</button>
-
-            {/* <button onClick={() => { navigate("/"); }}>
-                Home
-            </button> */}
-        </div>
+        <Row gutter={[16, 40]}>
+        <Col span = {8} offset = {16}>
+            <Dropdown
+                menu={{
+                items,
+            selectable: true,
+            defaultSelectedKeys: ['3'],
+            
+            
+            }}
+        >
+            <Typography.Link>
+             <Space>
+             Filter
+                <FilterFilled />
+                </Space>
+            </Typography.Link>
+        </Dropdown>   
+        </Col>           
+        </Row>
     );
 }
 
