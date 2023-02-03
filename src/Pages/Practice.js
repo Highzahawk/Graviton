f4731f6361d9ff2a50a7e574553aeace874b6f31
SameerisHere <104521530+SameerisHere@users.noreply.github.com>
2023-02-03 15:44:08 -0600

    created basic quiz selection page

--- src/Pages/Practice.js
+++ src/Pages/Practice.js
@@ -1,24 +1,51 @@
-import react from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import {DownloadOutlined, FilterFilled, SearchOutlined, PlusOutlined, DownOutlined} from '@ant-design/icons'
+import {Button, Layout, Space, Grid, Row, Col, Dropdown, Typography, Menu} from 'antd'
+import styles from "./PracticeStyles.css";
 
 function Practice(){
     const navigate = useNavigate();
+    
+    const handleUnitButtonClick = () => {
+        navigate('/Unit');
+      };
+
+    const classNames = (
+        <Menu>
+            <Menu.Item key="0">
+                AP Calculus AB
+            </Menu.Item>
+            <Menu.Item key="1">
+                AP Language
+            </Menu.Item>
+        </Menu>
+    );
 
     return(
-        <div>
-            <h>
-                Practice Page
-                <button>Select Class</button>
-            </h>
+        <Row gutter={[16, 40]}>
+           <Col span = {12}>
+                <Dropdown overlay={classNames} trigger={['hover']}>
+                    <Button type="primary" className = "ClassFilter" shape = "round" size = "large" >
+                        Class Filter <DownOutlined />
+                    </Button>
+                </Dropdown>
+            </Col>
 
-            <button onClick={() => { navigate("/QuizHome"); }}>
-                Quizzes
-            </button>
+            <Col span = {12}>
+                <Button type="primary" className = "Toolbar" shape = "round" size = "large" >
+                        <SearchOutlined style={{ fontSize: '24px' }} />
+    
+                </Button>
+            </Col>  
 
-            <button onClick={() => { navigate("/FlashcardHome"); }}>
-                Flash card sets
-            </button>
-        </div>
+            <Col span = {12}> 
+                <Button type = "default" className = "Units" size = "large" onClick={handleUnitButtonClick}>
+                Unit 1
+                </Button>
+            </Col>  
+     
+        </Row>
     );
 }
 
